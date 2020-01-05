bffa992123bed82741e14955ec0218d3859efc8b
Sandra Coburn <sandra_coburn@hotmail.com>
2020-01-04 18:27:24 -0800

    Updates

---
+++ precache-manifest.adee14af4dd41c0d233d654e72f4c1fc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbb2c0e6fe4ff67e30749a5e7899465f",
+    "url": "/ReactMovieApp/index.html"
+  },
+  {
+    "revision": "5f51bc7f805da066d143",
+    "url": "/ReactMovieApp/static/js/2.a06edd3c.chunk.js"
+  },
+  {
+    "revision": "14a645addc088f6bab27",
+    "url": "/ReactMovieApp/static/js/main.cda7852d.chunk.js"
+  },
+  {
+    "revision": "4be8f8b8f0a2aa6e302c",
+    "url": "/ReactMovieApp/static/js/runtime~main.8ee48dba.js"
+  },
+  {
+    "revision": "31f5bb1da5a478ef98a9213900c5f7a2",
+    "url": "/ReactMovieApp/static/media/no_image.31f5bb1d.jpg"
+  },
+  {
+    "revision": "08494abf4210d1ea8b163791db70773c",
+    "url": "/ReactMovieApp/static/media/reactMovie_logo.08494abf.png"
+  },
+  {
+    "revision": "30cd724bda43bdfa29216c8d8770b0c9",
+    "url": "/ReactMovieApp/static/media/tmdb_logo.30cd724b.svg"
+  }
+]);
